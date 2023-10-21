4730ea68f5a02a2c2c6b0a2c5b638c4ea0e29745
Petr Němec <xnemec93@vutbr.cz>
2023-10-21 22:11:29 +0200

    update scripts and data

--- scripts/MIDI_handler/MIDI_handler.py
+++ scripts/MIDI_handler/MIDI_handler.py
@@ -1,29 +1,160 @@
-import os
+﻿import os
 import sys
+import sounddevice as sd
+import numpy as np
 
+from scipy.io.wavfile import write
 from matplotlib import pyplot as plt
 from matplotlib import patches
 from matplotlib import colors
 import pretty_midi
 import pandas as pd
-import IPython.display as ipd
 
-sys.path.append('..')
+FMP_COLORMAPS = {
+    'FMP_1': np.array([[1.0, 0.5, 0.0], [0.33, 0.75, 0.96], [0.0, 1.0, 0.0], [1.0, 1.0, 0.0], [0.0, 1.0, 1.0],
+                       [1.0, 0.0, 1.0],  [0.99, 0.51, 0.71], [0.53, 0.0, 0.46], [0.56, 0.93, 0.72], [0, 0, 0.9]])
+}
 
-fn = os.path.join('..', 'data', 'C1', 'FMP_C1_F13a_Beethoven_FateMotive_Sibelius-Tracks.mid')
-midi_data = pretty_midi.PrettyMIDI(fn)
-midi_list = []
+def load_midi(fn=os.path.join('..', '..', 'data', 'MIDI', 'test.mid')):
+    sys.path.append('..')
+    midi_data = pretty_midi.PrettyMIDI(fn)
+    return midi_data
 
-for instrument in midi_data.instruments:
-    for note in instrument.notes:
-        start = note.start
-        end = note.end
-        pitch = note.pitch
-        velocity = note.velocity
-        midi_list.append([start, end, pitch, velocity, instrument.name])
-        
-midi_list = sorted(midi_list, key=lambda x: (x[0], x[2]))
+def visualize_piano_roll(score, xlabel='Time (seconds)', ylabel='Pitch', colors='FMP_1', velocity_alpha=False,
+                         figsize=(12, 4), ax=None, dpi=72):
+    """Plot a pianoroll visualization
 
-df = pd.DataFrame(midi_list, columns=['Start', 'End', 'Pitch', 'Velocity', 'Instrument'])
-html = df.to_html(index=False)
-ipd.HTML(html)
+    Notebook: C1/C1S2_CSV.ipynb
+
+    Args:
+        score: List of note events
+        xlabel: Label for x axis (Default value = 'Time (seconds)')
+        ylabel: Label for y axis (Default value = 'Pitch')
+        colors: Several options: 1. string of FMP_COLORMAPS, 2. string of matplotlib colormap,
+            3. list or np.ndarray of matplotlib color specifications,
+            4. dict that assigns labels  to colors (Default value = 'FMP_1')
+        velocity_alpha: Use the velocity value for the alpha value of the corresponding rectangle
+            (Default value = False)
+        figsize: Width, height in inches (Default value = (12)
+        ax: The Axes instance to plot on (Default value = None)
+        dpi: Dots per inch (Default value = 72)
+
+    Returns:
+        fig: The created matplotlib figure or None if ax was given.
+        ax: The used axes
+    """
+    fig = None
+    if ax is None:
+        fig = plt.figure(figsize=figsize, dpi=dpi)
+        ax = plt.subplot(1, 1, 1)
+
+    labels_set = sorted(set([note[4] for note in score]))
+    colors = color_argument_to_dict(colors, labels_set)
+
+    pitch_min = min(note[2] for note in score)
+    pitch_max = max(note[2] for note in score)
+    time_min = min(note[0] for note in score)
+    time_max = max(note[0] + note[1] for note in score)
+
+    for start, duration, pitch, velocity, label in score:
+        if velocity_alpha is False:
+            velocity = None
+        rect = patches.Rectangle((start, pitch - 0.5), duration, 1, linewidth=1,
+                                 edgecolor='k', facecolor=colors[label], alpha=velocity)
+        ax.add_patch(rect)
+
+    ax.set_ylim([pitch_min - 1.5, pitch_max + 1.5])
+    ax.set_xlim([min(time_min, 0), time_max + 0.5])
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.grid()
+    ax.set_axisbelow(True)
+    ax.legend([patches.Patch(linewidth=1, edgecolor='k', facecolor=colors[key]) for key in labels_set],
+              labels_set, loc='upper right', framealpha=1)
+
+    if fig is not None:
+        plt.tight_layout()
+
+    return fig, ax
+
+
+def color_argument_to_dict(colors, labels_set, default='gray'):
+    """Create a dictionary that maps labels to colors.
+
+    Args:
+        colors: Several options: 1. string of ``FMP_COLORMAPS``, 2. string of matplotlib colormap,
+            3. list or np.ndarray of matplotlib color specifications, 4. dict that assigns labels  to colors
+        labels_set: List of all labels
+        default: Default color, used for labels that are in labels_set, but not in colors
+
+    Returns:
+        color_dict: Dictionary that maps labels to colors
+    """
+
+    if isinstance(colors, str):
+        # FMP colormap
+        if colors in FMP_COLORMAPS:
+            color_dict = {l: c for l, c in zip(labels_set, FMP_COLORMAPS[colors])}
+        # matplotlib colormap
+        else:
+            cm = plt.get_cmap(colors)
+            num_labels = len(labels_set)
+            colors = [cm(i / (num_labels + 1)) for i in range(num_labels)]
+            color_dict = {l: c for l, c in zip(labels_set, colors)}
+
+    # list/np.ndarray of colors
+    elif isinstance(colors, (list, np.ndarray, tuple)):
+        color_dict = {l: c for l, c in zip(labels_set, colors)}
+
+    # is already a dict, nothing to do
+    elif isinstance(colors, dict):
+        color_dict = colors
+
+    else:
+        raise ValueError('`colors` must be str, list, np.ndarray, or dict')
+
+    for key in labels_set:
+        if key not in color_dict:
+            color_dict[key] = default
+
+    return color_dict
+
+
+def midi_to_list(midi):
+    """Convert a midi file to a list of note events
+
+    Notebook: C1/C1S2_MIDI.ipynb
+
+    Args:
+        midi (str or pretty_midi.pretty_midi.PrettyMIDI): Either a path to a midi file or PrettyMIDI object
+
+    Returns:
+        score (list): A list of note events where each note is specified as
+            ``[start, duration, pitch, velocity, label]``
+    """
+
+    if isinstance(midi, str):
+        midi_data = pretty_midi.pretty_midi.PrettyMIDI(midi)
+    elif isinstance(midi, pretty_midi.pretty_midi.PrettyMIDI):
+        midi_data = midi
+    else:
+        raise RuntimeError('midi must be a path to a midi file or pretty_midi.PrettyMIDI')
+
+    score = []
+
+    for instrument in midi_data.instruments:
+        for note in instrument.notes:
+            start = note.start
+            duration = note.end - start
+            pitch = note.pitch
+            velocity = note.velocity / 128.
+            score.append([start, duration, pitch, velocity, instrument.name])
+    return score
+
+score = midi_to_list(load_midi(
+    #fn=os.path.join('..', '..', 'data', 'MIDI', 'PokerFace.mid')
+    ))
+
+fig, ax = visualize_piano_roll(score, figsize=(12, 5), velocity_alpha=True);
+
+plt.show()
