207a6df3d4d1a7f3be6052c4816427d7ae120dd1
Petr Němec <xnemec93@vutbr.cz>
2023-11-27 22:53:22 +0100

    dtw testing

--- scripts/DP/SYNC/DTW.py
+++ scripts/DP/SYNC/DTW.py
@@ -23,10 +23,10 @@ import numpy as np
 
 # TODO: implement cost matrix, warping path etc. 
 def warping_path(X, Y):
-    D, wp = librosa.sequence.dtw(X, Y, backtrack=True)
+    D, wp = librosa.sequence.dtw(X, Y, subseq=True)
 
 
-    fig, ax = plt.subplots(nrows=2, sharex=True)
+    fig, ax = plt.subplots(nrows=2, sharex=False)
     img = librosa.display.specshow(D, x_axis='frames', y_axis='frames',
                                    ax=ax[0])
     ax[0].set(title='DTW cost', xlabel='Noisy sequence', ylabel='Target')
@@ -34,10 +34,12 @@ def warping_path(X, Y):
     ax[0].legend()
     fig.colorbar(img, ax=ax[0])
     ax[1].plot(D[-1, :] / wp.shape[0])
-    ax[1].set(xlim=[0, Y.shape[1]], ylim=[0, 2],
+    ax[1].set(xlim=[0, Y.shape[1]], #ylim=[0, 2],
               title='Matching cost function')
 
     plt.show()
+
+    return 0
 
 
 def test():
@@ -48,31 +50,31 @@ def test():
 
     Fs = 22050
     N = 4410
-    H = N//2
-    fn_wav_X = os.path.join('..', '..', 'data', 'audio', 'test.wav')
+    H = N//4
+    fn_wav_X = os.path.join('..', '..', 'data', 'audio', 'nev_gon_give_you_up.webm')
 
     X_wav, Fs = librosa.load(fn_wav_X, sr=Fs)
-    X = librosa.feature.chroma_stft(y=X_wav, sr=Fs, tuning=0, norm=2, hop_length=H, n_fft=N)
+    X = librosa.feature.chroma_stft(y=X_wav, sr=Fs, hop_length=H, n_fft=N)
 
-    path_midi =os.path.join('..', '..', 'data', 'MIDI', 'test.mid')
+    path_midi =os.path.join('..', '..', 'data', 'MIDI', 'nev_gon_giv_you_up.mid')
     Y = pm.PrettyMIDI.get_chroma(handle.load_midi(path_midi))
 
 
     
-    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(10, 6))
+    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(10, 6), sharex=False)
     img = librosa.display.specshow(X, x_axis='frames', y_axis='chroma', cmap='gray_r', hop_length=H, ax=ax[0])
     ax[0].set(title='Sequence $X$')    
     ax[0].set_xlabel('Time (frames)')
     ax[0].set_ylabel('Chroma')  
+    ax[0].label_outer()
 
     librosa.display.specshow(Y, x_axis='frames', y_axis='chroma', cmap='gray_r', hop_length=H, ax=ax[1])
     ax[1].set(title='Sequence $Y$')
     ax[1].set_xlabel('Time (frames)')
     ax[1].set_ylabel('Chroma')
+    ax[0].label_outer()
     
     fig.colorbar(img, ax=ax)
-
-    plt.tight_layout(); 
     plt.show()
 
     warping_path(X, Y)
