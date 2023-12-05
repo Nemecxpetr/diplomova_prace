710e185c2a047401ca063153eafdcd4ca0789617
Petr Němec <xnemec93@vutbr.cz>
2023-12-05 12:00:57 +0100

    WP testing dtw_test files

--- scripts/DP/SYNC/DTW.py
+++ scripts/DP/SYNC/DTW.py
@@ -89,13 +89,14 @@ def dtw_test(show=True):
     #test_audio, Fs = handle.read_audio(os.path.join('..', '..', 'data', 'audio', 'test.wav'))
     #handle.plot_signal_in_time(test_audio, Fs)
 
+    # I don't know why but I decided to downsample it
     Fs = 22050
-    N = 4410//2
-    H = N//4
-    fn_wav_X = os.path.join('..', '..', 'data', 'audio', 'DTW_test.wav')
+    N = 2048
+    H = N//2
+    fn_wav_x = os.path.join('..', '..', 'data', 'audio', 'DTW_test_whistle.wav')
 
-    X_wav, Fs = librosa.load(fn_wav_X, sr=Fs)
-    X = librosa.feature.chroma_stft(y=X_wav, sr=Fs, hop_length=H, n_fft=N)
+    x_wav, Fs = librosa.load(fn_wav_x, sr=Fs)
+    X = librosa.feature.chroma_stft(y=x_wav, sr=Fs, hop_length=H, n_fft=N)
 
     path_midi =os.path.join('..', '..', 'data', 'MIDI', 'DTW_test.mid')
     midi_data = handle.load_midi(path_midi)
@@ -121,8 +122,10 @@ def dtw_test(show=True):
     #NOTE: X = audio chroma, Y = score chroma
     wp = warping_path(X, Y, show=show)
 
-    #print(wp[0:10])
-    print(len(wp))
+    print(len(x_wav))
+    print(np.shape(X))
+    print(np.shape(Y))
+
     #NOTE: wp is ((x1, y1), (x2, y2), ..., (xn, ym))
     # What is x and y - is it time index or is it time in seconds? -> we need time in seconds preferebly so that we can adjust the time in csv file later
     # - if this is index we need to compute the time in seconds with the T_coef function (I could probably use the one in libfmp, or just quickly create one)
@@ -131,7 +134,6 @@ def dtw_test(show=True):
     #TODO: now we need to adjust the times in the original midi
 
     score = handle.midi_to_list(midi_data)
-    print (len(score))
     #TODO: problem!! warping path of the midi data is for the midi data of the chromagram which we need to adjust and then transform back into midi.
     # Question? How to do that?
     #  
@@ -139,14 +141,18 @@ def dtw_test(show=True):
     # the best thing to do would be to interpolate the warping path so that it is "continuous" 
     # (more on the struggle of interpolation later -> TODO)
     # then find new values for midi notes (original time values in the csv_from_midi object)
+    #
+    # If I don't want to interpolate other idea could be to use different warping path conditions
 
-    for n in range(len(score[0])):
-         print(score[0][n])
+
+
 
     # question how to adjust if more tones that originally start at the same time are now all different? How does the warping path reflect that?
+    # Now that I think musically about it how would I even align that as a human listener? so I guess it doesn't really matter. If it picks the best match (The WP with least cost)
+    # It should probably work
 
 
-dtw_test()
+dtw_test(show=True)
 
 
 """
