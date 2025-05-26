6f201894e29434eeef33d0701e36c964c3bbee1d
Petr Němec <xnemec93@vutbr.cz>
2025-05-26 10:27:46 +0200

    testing new beat evaluation function

--- scripts/DP/SYNC/evaluator.py
+++ scripts/DP/SYNC/evaluator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38e0d2bfeae904a765ed2d67f19e368803e9ce09f72d04aef43f9b5a1a6ca42a
-size 19883
+oid sha256:4c8f9218c551a0ad3b651cd1f118bbffa3546881a44ccbfa2070b72732c44f29
+size 20950
--- scripts/DP/main.py
+++ scripts/DP/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fc0eaaf2203944b8110674f7631dbfc5e4acc64f812c37f4b97ec3de4e795c9
-size 12710
+oid sha256:2f26c2b286d3b6b7445ea60a0330049d0e9e5470c42e101b34042e67792fa37c
+size 13503
